d63185162825ddd5ec3975163380b0404352bc16
Deep Tailor <deeptailor@gmail.com>
2017-04-05 00:05:43 -0700

    splash background, form style and cookie on click

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Splash extends React.Component{
   constructor(props){
     super(props);
   }
 
+  clickEnterChat(){
+    document.cookie = "";
+    let nickname = $('.nickname').val();
+    document.cookie = `name=${nickname}`;
+  }
+
   render(){
     return(
       <div className='splash-container'>
-        <form className="splash-form">
-          <input className="nickname" type="text" placeholder="Enter your Nickname"></input>
-        </form>
+        <div className="splash-overlay">
+          <h1>SEMA.chat</h1>
+          <form className="splash-form">
+            <input className="nickname" type="text" placeholder="Please Enter Your Nickname"></input>
+            <div className="enter-chat-button" onClick={this.clickEnterChat}>Enter Chat</div>
+          </form>
+        </div>
       </div>
     )
   }
