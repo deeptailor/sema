77beba8f3b7373a4db83078f0e6d4b44890a2aea
Deep Tailor <deeptailor@gmail.com>
2017-04-04 17:04:45 -0700

    add render method to splash component

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,6 +4,12 @@ class Splash extends React.Component{
   constructor(props){
     super(props);
   }
+
+  render(){
+    return(
+      <div className='splash-container'></div>
+    )
+  }
 }
 
 export default Splash;
