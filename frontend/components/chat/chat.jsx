4b1776eb68d005c3d5782749d346f1aff4c5b728
Deep Tailor <deeptailor@gmail.com>
2017-04-05 18:36:15 -0700

    style chat page

--- frontend/components/chat/chat.jsx
+++ frontend/components/chat/chat.jsx
@@ -5,14 +5,23 @@ class Chat extends React.Component{
     super(props);
   }
 
+  renderChatMessages(){
+    return(
+      <ul className="chat-messages"></ul>
+    );
+  }
+
   render(){
     return(
       <div className="chat-container">
-        Chat Room
+        {this.renderChatMessages()}
+        <div className="chat-input-container">
+          <input className="chat-input" placeholder="Enter Text Here"></input>
+          <div className="chat-submit">Send</div>
+        </div>
       </div>
     )
   }
-
 }
 
 export default Chat;
